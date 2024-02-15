cce2fe877bec4396f1d48c6358b0c7b19395c4af
eunyoung0305 <eunyoungpark0305@gmail.com>
2024-02-15 18:11:57 +0900

    css update

--- src/main/onedayseedjs/src/main/MainPage.js
+++ src/main/onedayseedjs/src/main/MainPage.js
@@ -18,33 +18,39 @@ const MainPage = ()=>{
                     </div >
                 </div>
                 <div className="body-box">
+                    <div className="body-wrap">
                     <div className="category-box">
+                        <div className="wrap">
                         <button type="button" className="category-header">카테고리</button>
                         <br/>
+                        <form className="category-btn">
                         <Link to="/lesson/list?q=요리">
-                            <button type="button" className="btn btn-success">요리</button>
+                            <button type="button" className="btn btn-success " id="btn1">요리</button>
                         </Link>
                         <Link to="/lesson/list?q=미술">
-                            <button type="button" className="btn btn-success">미술</button>
+                            <button type="button" className="btn btn-success " id="btn2">미술</button>
                         </Link>
                         <Link to="/lesson/list?q=음악">
-                            <button type="button" className="btn btn-success">음악</button>
+                            <button type="button" className="btn btn-success " id="btn3">음악</button>
                         </Link>
+                     
                         <br/>
                         <Link to="/lesson/list?q=베이킹">
-                            <button type="button" className="btn btn-success">베이킹</button>
+                            <button type="button" className="btn btn-success" id="btn4">베이킹</button>
                         </Link>
                         <Link to="/lesson/list?q=운동">
-                            <button type="button" className="btn btn-success">운동</button>
+                            <button type="button" className="btn btn-success " id="btn5">운동</button>
                         </Link>
                         <Link to="/lesson/list">
-                            <button type="button" className="btn btn-success"><b>전체 보기</b></button>
+                            <button type="button" className="btn btn-success " id="btn6"><b>전체 보기</b></button>
                         </Link>
-
+                        </form>
+                        </div>
                     </div>
 
                     <div className="mbti-box">
                         <a href="https://eunyoung0305.github.io/hobby_mbti/"><div className="mbti mbti-img"></div></a>
+                    </div>
                     </div>
                 </div>
             </BasicLaylout>
